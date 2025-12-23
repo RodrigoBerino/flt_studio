790d851d28e5c5027a84f855fa491897bdd70c7a
rodrigoberino <r9.berino@gmail.com>
2025-12-22 21:08:05 -0300

    Add sinc and http route

--- async/assincr/bin/assincr.dart
+++ async/assincr/bin/assincr.dart
@@ -1,5 +1,15 @@
-import 'package:assincr/assincr.dart' as assincr;
-
+import 'package:http/http.dart';
 void main(List<String> arguments) {
-  print('Hello world: ${assincr.calculate()}!');
+  requestData();
 }
+
+requestData(){
+  String url = "https://gist.githubusercontent.com/RodrigoBerino/e056f8fcb424ceffd620e4c59e3825b3/raw/82cd28caac756b58bac74b43d87db3087a956e5c/accounts.json";
+  Future <Response> futureResponse = get(Uri.parse(url));
+  print(futureResponse);
+  futureResponse.then((Response response) {
+    print(response);
+    print(response.body);
+  },);
+}
+
