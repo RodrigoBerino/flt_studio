12ba571015fcdb3ebf72390176fc364d5c52df43
rodrigoberino <r9.berino@gmail.com>
2026-01-15 19:54:44 -0300

    last update in asinc

---
+++ async/assincr/lib/exceptions/transaction_exceptions.dart
@@ -0,0 +1,11 @@
+class SenderNotExistsException implements Exception {
+
+}
+
+class ReceiverNotExistsException implements Exception{
+
+}
+
+class InsufficientFundsException implements Exception{
+  
+}
--- async/assincr/lib/services/transaction_service.dart
+++ async/assincr/lib/services/transaction_service.dart
@@ -1,3 +1,5 @@
+import 'package:assincr/exceptions/transaction_exceptions.dart';
+import 'package:assincr/helpers/helper_taxes.dart';
 import 'package:assincr/models/account.dart';
 import 'package:assincr/services/account_service.dart';
 
@@ -11,15 +13,38 @@ class TransactionService {
     required double amount,
   }) async {
     List<Account> listAccount = await _accountService.getAll();
+
+    if (listAccount.where((acc) => acc.id == idSender).isEmpty){
+      throw SenderNotExistsException();
+    }
     
     Account senderAccount =listAccount.firstWhere( //percorrer a lista e coloca o objeto no sender acc
       (acc) => acc.id == idSender,
     );
 
+    if (listAccount.where((acc) => acc.id == idReceiver).isEmpty){
+      throw ReceiverNotExistsException();
+
+    }
+
     Account receiverAccount = listAccount.firstWhere( //percorrer a lista e coloca o objeto no sender acc
       (acc) => acc.id == idReceiver,
     );
 
+    double taxes = calculateTaxesByAccount(
+      sender: senderAccount,
+      amount: amount
+    );
 
+    if (senderAccount.balance < amount + taxes){
+      throw InsufficientFundsException();
+    }
+
+    senderAccount.balance -= (amount + taxes);
+    receiverAccount.balance += amount;
+
+    listAccount[listAccount.indexWhere(
+      (acc) => acc.id == senderAccount.id,
+    )] = senderAccount;
   }
 }
